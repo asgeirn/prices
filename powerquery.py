697541a6899c14b3e2629749f3ec5e044785ad4e
Asgeir Storesund Nilsen <asgeir.nilsen@miles.no>
2022-12-01 18:21:28 +0100

    Pandas 0.23.3 does not handle tz in read_json

--- powerquery.py
+++ powerquery.py
@@ -18,7 +18,9 @@ start = datetime.datetime.combine(day, datetime.time.min)
 stop = datetime.datetime.combine(day, datetime.time.max)
 
 pricefile = pathlib.Path(f"{day}.json")
-prices = pandas.read_json(path_or_buf=pricefile, orient="index", typ="series")
+prices = pandas.read_json(
+    path_or_buf=pricefile, orient="index", typ="series"
+).tz_localize("UTC")
 # print(prices)
 
 client = InfluxDBClient.from_env_properties()
